b0998f6a2b018d930d98411c4ea1b8c44f0e1203
Ederlei Silva <ederleipereiras1@hotmail.com>
2024-12-12 14:49:27 -0300

    Adicionado estilo no default login layout e criado a logica de login na class de login

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
+import { FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    DefaultLoginLayoutComponent,
+    ReactiveFormsModule
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  loginForm!: FormGroup;
+
+  constructor(){
+    this.loginForm = new FormGroup({
+      email: new FormGroup('', [Validators.required, Validators.email]),
+      password: new FormGroup('', [Validators.required, Validators.minLength(6)])
+    });
+  }
+
+}
